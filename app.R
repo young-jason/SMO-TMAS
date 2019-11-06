3f899993ec0dc7b70b18a5a497e2b56d4c58e62c
Jason Young <55795533+young-jason@users.noreply.github.com>
2019-11-06 18:05:50 +0100

    Update app.R

--- app.R
+++ app.R
@@ -2,7 +2,7 @@
 ##################################################################
 # Social Media Observatory - Twitter Mining and Analysis Suite [SMO-TMAS]
 # Jason Young
-# 24 October 2019
+# 05 November 2019
 ##################################################################
 ##################################################################
 
@@ -33,21 +33,20 @@ if(!require('stopwords')) install.packages('stopwords')
 library('stopwords') #multilingual stopword lists
 if(!require('RColorBrewer')) install.packages('RColorBrewer')
 library('RColorBrewer') #color palettes used for word cloud
-
+if(!require('DT')) install.packages('DT')
+library('DT') #creating interactive tables that can display on HTML pages
+if(!require('crosstalk')) install.packages('crosstalk')
+library('crosstalk') # advanced interaction controls for the HTML tables
 
 # shiny related library
 if(!require('shiny')) install.packages('shiny')
 library('shiny')
 if(!require('shinydashboard')) install.packages('shinydashboard')
 library('shinydashboard')
-if(!require('DT')) install.packages('DT')
-library('DT') #creating interactive tables that can display on HTML pages
-if(!require('crosstalk')) install.packages('crosstalk')
-library('crosstalk')
 
 # Twitter authentication
 token <- create_token(
-  app = "Delibration index",
+  app = "PLACEHOLDER",
   consumer_key = 'PLACEHOLDER',
   consumer_secret = 'PLACEHOLDER',
   access_token = 'PLACEHOLDER',
@@ -67,7 +66,8 @@ pull_tweets <- function(twitter_handle, n){
                   day_of_week = wday(created_at, label = TRUE),                          # Converting created_at to day of the week 
                   hour = hour(created_at),                                               # Converting created_at to hour  
                   month = month(created_at))                                             # Converting created_at to month 
-  incProgress(message = "Finished collecting tweets!")                               # Prompting the user that tweet collection has finished
+  incProgress(message = "Finished collecting tweets!")                                   # Prompting user that dara collection has finished
+  showNotification("Data has been collected - Please change tab to see analysis!", duration = 10, type = "message")   # Prompting the user to change tab
   return(tweets)
   })
 }
@@ -87,8 +87,28 @@ pull_topics <- function(twitter_topic, n){
                   day_of_week = wday(created_at, label = TRUE),                          # Converting created_at to day of the week  
                   hour = hour(created_at),                                               # Converting created_at to hour   
                   month = month(created_at))                                             # Converting created_at to month 
-  incProgress(message = "Finished collecting tweets!")                                   # Prompting the user that tweet collection has finished
+  incProgress(message = "Finished collecting tweets!")                                   # Prompting user that data collection has finished
+  showNotification("Data has been collected - Please change tab to see analysis!", duration = 10, type = "message")   # Prompting the user to change tab
   return(tweets)
+  })
+}
+
+# Setting-up a function to harvest live tweets of keywords / hashtags and arranging the results into a data frame 
+pull_stream <- function(){
+  withProgress(message = "Collecting tweets...", value = 0, {                            # Prompting the user that tweet collection has started
+    tweets <- get_trends(stream_topic                                             # stream_topic = text input by user in text box
+            ) %>%                                               # n =  specified number of tweets by user                                 
+      dplyr::select(screen_name, description, text, created_at, retweet_count,             # Selecting useful rows
+                    favorite_count, source, hashtags, urls_expanded_url, media_type, 
+                    mentions_screen_name, followers_count, friends_count, statuses_count, 
+                    favourites_count, account_created_at, verified) %>% 
+      dplyr::mutate(retweet_favorite_ratio = retweet_count/favorite_count,                 # Calculating retweet/favorite ratio
+                    day_of_week = wday(created_at, label = TRUE),                          # Converting created_at to day of the week  
+                    hour = hour(created_at),                                               # Converting created_at to hour 
+                    month = month(created_at))                                             # Converting created_at to month              
+    incProgress(message = "Finished collecting tweets!")                                   # Prompting  user that data collection has finished
+    showNotification("Data has been collected - Please change tab to see analysis!", duration = 10, type = "message")   # Prompting the user to change tab
+    return(tweets)
   })
 }
 
@@ -99,7 +119,6 @@ tweets_per_day <- function(temp_tweets){
                   y = freq_table$Freq,
                   type = "bar") %>% 
     layout(title = "Tweets per Day of the Week")
-  
   return(plot)
 }
 
@@ -161,7 +180,7 @@ word_frequency <- function(temp_tweets){
   PostsCorpus <- corpus(PostsCorpus, text_field='text')   # Creating corpus of tweet text
   Corpus <- dfm(PostsCorpus, tolower = T, stem = T, remove_punct = T, remove_twitter = T, remove_numbers = T,
                 remove_symbols = T, remove_separators = T, remove_hyphens = T, remove_url = T,
-                remove = c(stopwords("en"), stopwords("de"), stopwords("fr"), stopwords("it"), 'rt'))
+                remove = c(stopwords("en"), stopwords("de"), stopwords("fr"), stopwords("it"), 'rt', 'na'))
   #Creating dfm and cleaning text: removing German, English, French and Italian stopwords and words with low information value
   frequency <- textstat_frequency(Corpus)              # word frequency table
   return(frequency)
@@ -200,8 +219,6 @@ ui <- fluidPage(
     sidebarPanel(width = 2,
       # Get timelines
       h3("Get Timelines", align = "center"),
-      helpText(
-        ("Returns up to 3,200 tweets"), br("of specified Twitter users."), align = "center"),
       # text input for Twitter handles
       textInput(
         inputId = "twitter_handle",
@@ -214,21 +231,18 @@ ui <- fluidPage(
         inputId = "tweet_num",
         label = "Number of Tweets",
         min = 0, max = 3200,
-        value = 3200,
+        value = 1600,
         width = 200
       ),
       # action button to scrape users
       actionButton(
         inputId = "click",
         width = 175,
-        label = ("Download tweets"), br("of specific users"),
+        label = ("Collect user timelines"),
         icon = icon("users", lib = "font-awesome")
       ),
       # Search tweets
       h3("Search Tweets", align = "center"),
-      #help text
-      helpText(
-        ("Returns up to 18,000"), br("tweets containing specified keywords."), align = "center"),
       # text input for keywords / hashtags 
       textInput(
         inputId = "twitter_topic",
@@ -241,40 +255,67 @@ ui <- fluidPage(
         inputId = "topic_num",
         label = "Number of Tweets",
         min = 0, max = 18000,
-        value = 18000,
+        value = 9000,
         width = 200
       ),
       # action button to scrape hashtags
       actionButton(
         inputId = "click2",
         width = 175,
-        label = ("Download tweets"), br("with specific keywords"),
+        label = ("Collect tweets from"), br("the past 6-9 days"),
         icon = icon("hashtag", lib = "font-awesome")
+      ),
+      h3("Stream Tweets", align = "center"),
+      # text input for keywords / hashtags for stream API (live tweets)
+      textInput(
+        inputId = "stream_topic",
+        label = "Enter a Keyword",
+        width = 300,
+        value = "NBA"
+      ),
+      # slider input
+      sliderInput(
+        inputId = "stream_time",
+        label = "Number of Seconds",
+        min = 0, max = 40,
+        value = 20
+      ),
+      # action button to scrape live tweets
+      actionButton(
+        inputId = "click3",
+        width = 175,
+        label = ("Collect live tweets"), 
+        icon = icon("hashtag", lib = "font-awesome")        
       )
     ),
     mainPanel(
       tabsetPanel(
       tabPanel("About",
-      h2('Social Media Observatory - Twitter Mining and Analysis Suite [SMO-TMAS]', align = "center"), p(''),
+      h2('Social Media Observatory - Twitter Mining and Analysis Suite [SMO-TMAS]', align = "center"),
       p(em('Leibniz-Institut für Medienforschung | Hans-Bredow-Institut'), align = "center"), p(em('Social Media Observatory'), align = "center"), 
-      p('Users are invited to enter a Twitter handle (no @ required) or keyword / hashtag (no # required) and specify a number of tweets to harvest.',
-        style='font-family:arial; font-si16pt; color:red', align = "center"), 
-      p('The second tab', span('Tweets', style='color:blue'), 'shows a searchable and downloadable table of the scraped Tweets.', align = "center"),
-      p('The third tab', span('Statistics', style ='color:blue'), 'shows interactive charts to analyse the meta data of the scraped Tweets.', align = "center"), 
-      p('The fourth tab', span('Text Analysis', style='color:blue'), 'shows a word cloud and word frequency table to analyse the text of the scraped Tweets.', align = "center"),
-      p('The fifth tab', span('Network Analysis', style='color:blue'), 'shows network graphs to analyse the 50 most frequently mentioned users and hashtags of the scraped Tweets.', align = "center"),
-      p(''),
-      p(em('NOTICE: The app gathers public data from Twitter’s REST API by querying the GET statuses/user_timeline or search/tweets endpoint.
-           There is a limit of 100,000 requests per day to the statuses/user_timeline endpoint (Get Timelines).
-           The search/tweets endpoint (Search Tweets) only returns data from the past 6-9 days with a focus on relevancy and not completeness. 
-           The scraped data is equivalent to the information publicly seen on Twitter (i.e.tweets belonging to protected users are not returned).
-           The number of tweets returned can be less than what was specified or the connection can terminate (i.e. server connection lost - reload). 
+      HTML('<hr style="color: black;">'),
+      p(strong('INSTRUCTIONS:'), align = "center"),
+      p('Users are invited to enter a Twitter handle (no @ required) or keyword / hashtag (no # required) and specify a number of posts or seconds to stream.', align = "center"), 
+      p('The second tab', span('Tweets', style='color:red'), 'shows a table of the collected Tweets that can be filtered and downloaded.', align = "center"),
+      p('The third tab', span('Statistics', style ='color:red'), 'shows interactive charts to analyse the meta data of the scraped Tweets.', align = "center"), 
+      p('The fourth tab', span('Text Analysis', style='color:red'), 'shows a word cloud and word frequency table to analyse the text of the scraped Tweets.', align = "center"),
+      p('The fifth tab', span('Network Analysis', style='color:red'), 'shows network graphs to analyse the 50 most frequently mentioned users and hashtags of the scraped Tweets.', align = "center"),
+      p(strong('DATA GATHERING:'), align = "center"),
+      p(span('Get Timelines:', style ='color:red'), "By querying Twitter's REST API GET statuses/user_timeline endpoint the app returns up to 3,200 statuses posted to the timelines of specified Twitter users. 
+        Twitter rate limits cap the number of search requests to 100,000 per day.", align = "center"),
+      p(span('Search Tweets:', style='color:red'), "By querying Twitter's REST API GET search/tweets endpoint the app returns tweets containing specified keywords from the past 6-9 days with a focus on relevancy and not completeness. 
+        Twitter rate limits cap the number of search results returned to 18,000 every 15 minutes.", align = "center"), 
+      p(span('Stream Tweets:', style='color:red'), "By querying Twitter's STREAM API the app returns tweets containing specified keywords from the live stream of Twitter data.
+        Twitter rate limits cap the number of search results returned to 1% of the total volume of tweets at that moment.
+        In other words, as long as the filter query accounts for less than 1% of the total pool of data, every single tweet containing the matching keywords is received. 
+        This 1% sample includes up to 58 tweets per second as Twitter is one of the largest social networks with more than 500 million tweets per day.", align = "center"),
+      p(strong('NOTICE:'), align = "center"),
+           p(em('All of the collected data is equivalent to the information publicly seen on Twitter (i.e.tweets belonging to protected users are not returned).
+           The number of tweets returned can be less than what was specified or the connection can terminate. 
            This can happen because (a) the search query did not return many results (the search pool is already thinned out from the population of tweets to begin with), 
-           (b) because user hitting the rate limit, or (c) of recent activity 
-           (either more tweets, which affect pagination in returned results or deletion of tweets).
-           The app should be used in strict accordance with', tags$a(href="https://developer.twitter.com/en/developer-terms/more-on-restricted-use-cases",
-                                                                         'Twitter’s developer terms.')), align = "center"),
-      p(''),
+           (b) because users are hitting the rate limit, or (c) of recent activity (either more tweets, which affect pagination in returned results or deletion of tweets).
+           SMO-TMAS should be used in strict accordance with', tags$a(href="https://developer.twitter.com/en/developer-terms/more-on-restricted-use-cases", 'Twitter’s developer terms,'), 
+           'which means that the app shall not be used to derive sensitive characteristics about Twitter users, off-Twitter matching, redistribution of Twitter content or any other acts that violate Twitter policies.'), align = "center"),
       tags$p("This app was created with", HTML("&#x2764;&#xFE0F;"), "+",  HTML("\u2615\uFE0F"), "by", tags$a(href = 'https://www.linkedin.com/in/jasonyoung4/', 'Jason Young'),
              'with',  HTML("&#x1F4AA;"), 'from',
              tags$a(href = 'http://rtweet.info/', 'rtweet'), ',',
@@ -296,7 +337,7 @@ ui <- fluidPage(
     "Statistics",
     fluidPage(
       h2("Charts of scraped Tweets", align = "center"),
-      p("The interactive charts below visualize the meta data of the scraped Tweets", align = "center"),
+      p("The interactive charts below visualize the meta data of the scraped Tweets.", align = "center"),
       box(plotlyOutput(outputId = "tweets_per_day")),
       box(plotlyOutput(outputId = "tweets_per_hour")),
       box(plotlyOutput(outputId = "metric_histograms")),
@@ -332,7 +373,7 @@ ui <- fluidPage(
 ### 2) Server Component ###
 server <- function(input, output, ...){
   
-  #### User Tweets
+## Get Timelines ##
   observeEvent(input$click, {
     # Defining user handle and sample size
     handle <- isolate({as.character(input$twitter_handle)})
@@ -363,7 +404,7 @@ server <- function(input, output, ...){
       tweet_source(temp_tweets())
     })
     
-    # Creating table of scraped Tweets
+    # Creating an interactive table of the collected Tweets
     m <- temp_tweets() %>% 
       tibble::rownames_to_column()
     d <- SharedData$new(m, ~rowname)
@@ -378,8 +419,8 @@ server <- function(input, output, ...){
                         color = DT::styleEqual(m2$rowname, rep("white", length(m2$rowname))),
                         backgroundColor = DT::styleEqual(m2$rowname, rep("blue", length(m2$rowname))))
       }
-    # Download table as .csv and creating a function to use input twitter handle and system date as file name
     })
+    # Download table as .csv and creating a function to use input twitter handle and system date as file name
     output$download <- downloadHandler(filename = function() {paste(input$twitter_handle, Sys.Date(), '.csv', sep='_')},
                                        content = function(file){           
                                          save_as_csv(temp_tweets(), file)
@@ -405,8 +446,8 @@ server <- function(input, output, ...){
       top_hashtags(temp_tweets())
       })
     })  
-  
-  #### Topic Tweets
+
+## Search Tweets ##
   observeEvent(input$click2, {
     # Defining topic and sample size
     handle <- isolate({as.character(input$twitter_topic)})
@@ -479,7 +520,79 @@ server <- function(input, output, ...){
       top_hashtags(temp_tweets())
       })
   })
-  }
+
+## Stream Tweets ##
+observeEvent(input$click3, {
+  # defining user handle and sample size
+  handle <- isolate({as.character(input$stream_topic)})
+  sample_size <- isolate({input$stream_time})
+  
+  # pulling tweets
+  temp_tweets <- reactive({
+    pull_stream(handle, sample_size)
+  })
+  
+  # bar chart depicting the days of the week that the tweets were created
+  output$tweets_per_day <- renderPlotly({
+    tweets_per_day(temp_tweets())
+  })
+  
+  # line plot depicting the time at which the tweets were created
+  output$tweets_per_hour <- renderPlotly({
+    tweets_per_hour(temp_tweets())
+  })
+  
+  #histogram depicting the distributions of the retweet count and favorited count metrics
+  output$metric_histograms <- renderPlotly({
+    distributions(temp_tweets())
+  })
+  
+  # pie graph depicting the source from which the tweets were sent:
+  output$tweet_source <- renderPlotly({
+    tweet_source(temp_tweets())
+  })
+  
+  # table
+  m <- temp_tweets() %>% 
+    tibble::rownames_to_column()
+  d <- SharedData$new(m, ~rowname)
+  
+  output$x1 <- DT::renderDataTable({
+    m2 <- m[d$selection(),]
+    dt <- DT::datatable(m)
+    if (NROW(m2) == 0) {
+      dt
+    } else {
+      DT::formatStyle(dt, "rowname", target = "row",
+                      color = DT::styleEqual(m2$rowname, rep("white", length(m2$rowname))),
+                      backgroundColor = DT::styleEqual(m2$rowname, rep("blue", length(m2$rowname))))
+    }
+  })
+  output$download <- downloadHandler(filename = function() {paste(input$handle, 'tweets.csv', sep='')},
+                                     content = function(file){
+                                       write.csv(temp_tweets(), file)
+                                     })
+  # word cloud
+  output$tweets_cloud <- renderPlot({
+    tweets_cloud(temp_tweets())
+  })
+  
+  # Creating frequency table
+  output$x2 <- DT::renderDataTable({
+    word_frequency(temp_tweets())
+  })
+  
+  # Creating user network
+  output$user_network <- renderPlot({
+    top_users(temp_tweets())
+  })
+  
+  # Creating hashtag network
+  output$hashtag_network <- renderPlot({
+    top_hashtags(temp_tweets())
+  })
+})
+}
 
 ### 3) shinyApp component ###
 shinyApp(ui = ui, server = server)
